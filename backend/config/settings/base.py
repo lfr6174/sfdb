a2e845fdef2006c63b1bbb57eb61b8db96d0a376
lfr6174 <lfr6174@gmail.com>
2026-06-13 13:22:38 +0800

    feat(backend): upgrade password hasher to Argon2 for enhanced security

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -97,6 +97,11 @@ AUTHENTICATION_BACKENDS = [
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
